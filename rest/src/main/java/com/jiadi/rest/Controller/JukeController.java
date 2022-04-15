ce2cfa6385f917590f2a7ddc13c2870349a4deb1
yujd10 <73360330+yujd10@users.noreply.github.com>
2022-04-15 18:50:56 -0400

    working on search by settingId

--- rest/src/main/java/com/jiadi/rest/Controller/JukeController.java
+++ rest/src/main/java/com/jiadi/rest/Controller/JukeController.java
@@ -1,11 +1,14 @@
 package com.jiadi.rest.Controller;
 
+import com.jiadi.rest.Model.Component;
 import com.jiadi.rest.Model.Juke;
 import com.jiadi.rest.Model.Setting;
 import com.jiadi.rest.Model.Setting__1;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import javax.websocket.server.PathParam;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
@@ -31,7 +34,18 @@ public class JukeController {
         return Arrays.asList(jukes);
     }
 
-    @RequestMapping("/jukes/{id}")
+    @RequestMapping("/jukes/settingid/{settingId}")
+    public List<Juke> getJukeBySettingId(@PathVariable("settingId") String settingId){
+        List<Juke> jukes = getAllJukes();
+        Setting settings = getAllSettings();
+
+        List<Juke> jukesFound = new ArrayList<>();
+        
+
+        return jukesFound;
+    }
+
+    @RequestMapping("/jukes/id/{id}")
     public Juke getJukeById(@PathVariable("id") String id){
         RestTemplate restTemplate = new RestTemplate();
 
@@ -39,16 +53,32 @@ public class JukeController {
         return juke;
     }
 
+
+
+    @RequestMapping("/jukes/model/{model}")
+    public Juke getJukeByModel(@PathVariable("model") String model){
+        RestTemplate restTemplate = new RestTemplate();
+
+        Juke[] jukes = restTemplate.getForObject(uriJuke, Juke[].class);
+        for (int i = 0; i< jukes.length;i++){
+            if (jukes[i].getModel().equals(model)){
+                return jukes[i];
+            }
+        }
+
+        return null;
+    }
+
     @RequestMapping("/settings")
-    public List<Setting> getAllSettings(){
+    public Setting getAllSettings(){
 
         RestTemplate restTemplate = new RestTemplate();
 
         Setting settings = restTemplate.getForObject(uriSetting, Setting.class);
-        return Arrays.asList(settings);
+        return settings;
     }
 
-    @RequestMapping("/settings/{id}")
+    @RequestMapping("/settings/id/{id}")
     public Setting__1 getSettingById(@PathVariable("id") String id){
         RestTemplate restTemplate = new RestTemplate();
 
